c0eb5b526a4869af3f3aac5b9c6329637a1015fd
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-05-28 21:15:20 -0500

    Initial Commit

---
+++ GradienteSwiftUI/GradienteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradienteSwiftUIApp.swift
+//  GradienteSwiftUI
+//
+//  Created by Oscar David Myerston Vega on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GradienteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
